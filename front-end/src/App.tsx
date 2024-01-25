ddfed8863280325335a2ab468155ae91d4457d31
Virajisesiri <viraji.sesiri@gmail.com>
2024-01-25 23:46:20 +0530

    Update main component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,16 @@
 import './App.css'
+import {useUser, useUserDispatcher} from "./context/UserContext.tsx";
 
 function App() {
+
+    const user = useUser();
+    const userDispatcher = useUserDispatcher();
+
+    if(user) {
+        userDispatcher({type: 'sign-in'});
+    }else {
+        userDispatcher({type: 'sign-out'});
+    }
 
 
   return (
