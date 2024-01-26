20651b10edc47f4bfc7ef787ba2eb8229ba39ab0
Virajisesiri <viraji.sesiri@gmail.com>
2024-01-26 21:59:47 +0530

    Update signIn.tsx and firebase.ts

--- front-end/src/signin/SignIn.tsx
+++ front-end/src/signin/SignIn.tsx
@@ -1,6 +1,19 @@
 import './SignIn.css';
+import {signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+import {auth} from "../firebase.ts";
 export function SignIn() {
+
+    function handleSignIn() {
+        signInWithPopup(auth, new GoogleAuthProvider());
+    }
     return (
-        <></>
+        <>
+            <div className="d-flex vh-100 flex-column gap-2 align-items-center justify-content-center">
+                <h1>Welcome to To-do App</h1>
+                <button onClick={handleSignIn} className="btn btn-primary">
+                    Sign in with Google
+                </button>
+            </div>
+        </>
     );
 }
