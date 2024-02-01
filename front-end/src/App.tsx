b5e6c13ead022090ffebacd27bbe3df991306d38
Virajisesiri <viraji.sesiri@gmail.com>
2024-02-01 10:22:14 +0530

    Modified App.tsx

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,22 +1,40 @@
 import './App.css'
 import {useUser, useUserDispatcher} from "./context/UserContext.tsx";
+import {onAuthStateChanged} from 'firebase/auth';
 import {SignIn} from "./signin/SignIn.tsx";
+import {Loader} from "./loader/Loader.tsx";
+import {useEffect, useState} from "react";
+import {auth} from "./firebase.ts";
 
 function App() {
-
+    const [loader, setLoader] = useState(true);
     const user = useUser();
     const userDispatcher = useUserDispatcher();
 
-    if(user) {
-        userDispatcher({type: 'sign-in'});
-    }else {
-        userDispatcher({type: 'sign-out'});
-    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setLoader(false);
+            if (user) {
+                userDispatcher({type: 'sign-in', user});
+            } else {
+                userDispatcher({type: 'sign-out'});
+            }
+        });
+        return () => unsubscribe();
+    }, []);
 
 
-  return (
+
+
+
+    return (
     <>
-      <SignIn/>
+        {loader ?
+            <Loader/>
+            :
+
+                <SignIn/>
+        }
     </>
   )
 }
